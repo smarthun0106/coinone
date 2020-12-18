6de0aa95a856aa61e82a9056acaf193a1806606c
smarthun0106 <smarthun0106@gmail.com>
2020-12-18 23:13:01 +0900

    updated

--- main.py
+++ main.py
@@ -2,19 +2,31 @@ from qttp.trading_apis import CoinoneApi
 import schedule
 import time
 
-def coinone_long_investment():
+def long_investment(market, invest_money):
     access          = "9130a983-8590-4beb-8a30-9b59d5b17e23"
     secret          = "f846bc98-5a74-4394-ba36-3c71b3f7b20f"
-    market          = "KLAY/KRW"
-    invest_money    = 7000
+    market          = market
+    invest_money    = invest_money
 
     trading = CoinoneApi(access, secret, market)
     ask_price = trading.ask_price(0)
     amount = round(invest_money / ask_price, 4)
     buy = trading.limit_buy(amount=amount, price=ask_price)
 
+
+def klay_long_investment():
+    market          = "KLAY/KRW"
+    invest_money    = 5000
+    long_investment(market, invest_money)
+
+def xrp_long_investment():
+    market          = "XRP/KRW"
+    invest_money    = 5000
+    long_investment(market, invest_money)
+
 if __name__ == "__main__":
-    schedule.every().day.at("14:01").do(coinone_long_investment)
+    schedule.every().day.at("14:01").do(klay_long_investment)
+    schedule.every().day.at("15:04").do(xrp_long_investment)
 
     while True:
         schedule.run_pending()
