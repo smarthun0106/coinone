8861237b861e97b287436190016664805630bc32
smarthun0106 <smarthun0106@gmail.com>
2020-12-31 07:43:50 +0900

    updated

--- main.py
+++ main.py
@@ -13,31 +13,52 @@ def long_investment(market, invest_money):
     amount = round(invest_money / ask_price, 4)
     buy = trading.limit_buy(amount=amount, price=ask_price)
 
-def klay_long_investment():
-    market          = "KLAY/KRW"
-    invest_money    = 4000
+def btc_long_investment():
+    market          = "BTC/KRW"
+    invest_money    = 3000
+    long_investment(market, invest_money)
+
+def eth_long_investment():
+    market          = "ETH/KRW"
+    invest_money    = 3000
     long_investment(market, invest_money)
 
 def xrp_long_investment():
     market          = "XRP/KRW"
-    invest_money    = 4000
+    invest_money    = 1000
     long_investment(market, invest_money)
 
 def xlm_long_investment():
     market          = "XLM/KRW"
-    invest_money    = 4000
+    invest_money    = 1000
     long_investment(market, invest_money)
 
 def bch_long_investment():
     market          = "BCH/KRW"
-    invest_money    = 4000
+    invest_money    = 3000
     long_investment(market, invest_money)
 
+def ltc_long_investment():
+    market          = "LTC/KRW"
+    invest_money    = 3000
+    long_investment(market, invest_money)
+
+def klay_long_investment():
+    market          = "KLAY/KRW"
+    invest_money    = 2000
+    long_investment(market, invest_money)
+
+
+
 if __name__ == "__main__":
-    schedule.every().day.at("09:05").do(klay_long_investment)
-    schedule.every().day.at("09:06").do(xrp_long_investment)
-    schedule.every().day.at("09:07").do(xlm_long_investment)
-    schedule.every().day.at("09:08").do(bch_long_investment)
+    schedule.every().day.at("09:01").do(btc_long_investment)
+    schedule.every().day.at("09:02").do(eth_long_investment)
+    schedule.every().day.at("09:03").do(xrp_long_investment)
+    schedule.every().day.at("09:04").do(xlm_long_investment)
+    schedule.every().day.at("09:05").do(bch_long_investment)
+    schedule.every().day.at("09:06").do(ltc_long_investment)
+    schedule.every().day.at("09:07").do(klay_long_investment)
+
 
     while True:
         schedule.run_pending()
